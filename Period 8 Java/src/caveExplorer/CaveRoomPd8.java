58fa8213e2605b82504425ee0b56b5dc3a5a6453
Student 8 <Student 8@10.8.33.147>
2016-10-31 14:02:41 -0400

    fasd

--- Period 8 Java/src/caveExplorer/CaveRoomPd8.java
+++ Period 8 Java/src/caveExplorer/CaveRoomPd8.java
@@ -5,5 +5,15 @@ public class CaveRoomPd8 {
 	public CaveRoomPd8(String description) {
 		// TODO Auto-generated constructor stub
 	}
+
+	public void enter() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setConnection() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
