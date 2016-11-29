0495324fd34cd3488ecc75970000fc7816d45fb8
Student 8 <Student 8@10.8.33.147>
2016-11-29 13:18:54 -0500

    dohgopetjhioth

--- Period 8 Java/src/pokemanSort/RecursionIntro.java
+++ Period 8 Java/src/pokemanSort/RecursionIntro.java
@@ -6,16 +6,32 @@ public class RecursionIntro {
 		// TODO Auto-generated method stub
 		System.out.println("Using 'For-Loop'");
 		for(int i = 0; i < 5; i++){
-			System.out.println("Hello World");
+			System.out.println("Hello World" + " x" + i);
 		}
 		System.out.println("Without using 'For-Loop'");
 		forLoop(5, new Action() {
 			
+			private int value = 0;
+			
 			public void act() {
 				// TODO Auto-generated method stub
-				System.out.println("Hello World!");
+				System.out.println("Hello World!"+ " x" + value);
+				value++;
 			}
 		});
+	}
+
+	private static void forLoop(int i, Action action) {
+		// TODO Auto-generated method stub
+		if(i <= 0){
+			//base case
+			return;
+		}else{
+			//standard action
+			action.act();
+			//recursive call
+			forLoop(i-1,action);
+		}
 	}
 
 }
