46eab410e4bc519133e3696272e75c0442ff3266
Student 8 <Student 8@10.8.33.147>
2016-12-06 14:03:19 -0500

    fdf

--- Period 8 Java/src/sort8/SortingAlgorithms.java
+++ Period 8 Java/src/sort8/SortingAlgorithms.java
@@ -7,11 +7,13 @@ public class SortingAlgorithms {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arrayToSort={133,0,-5,31,12,12,2};
-		System.out.println("- - - SELECTION SORT - - -");
+//		System.out.println("- - - SELECTION SORT - - -");
+//		System.out.println(Arrays.toString(arrayToSort));
+//		selectionSort(copy(arrayToSort));
+//		System.out.println("- - - BUBBLE SORT - - -");
+//		bubbleSort(copy(arrayToSort));
+		sort11(arrayToSort);
 		System.out.println(Arrays.toString(arrayToSort));
-		selectionSort(copy(arrayToSort));
-		System.out.println("- - - BUBBLE SORT - - -");
-		bubbleSort(copy(arrayToSort));
 	}
 	
 	public static int[] copy(int[] arr){
@@ -29,6 +31,22 @@ public class SortingAlgorithms {
 		arr[i]=placeHolder;
 		System.out.println(Arrays.toString(arr));
 	}
+	public static void sort11(int[] array){
+		System.out.println(java.util.Arrays.toString(array));
+	   for(int i=0;i<array.length; i++){
+	        for(int j = 0; j<array.length -1- i; j++){
+	        	if(array[j]>array[j+1]){
+	                int temp = array[j];
+	                System.out.println("looking at j: " + array[j] + " and j+1: " + array[j+1]);
+	                array[j]=array[j+1];
+	                array[j+1]=temp;
+	                System.out.println("current j: " + array[j] + " current j+1:" + array[j+1]);
+	            }
+	        	
+	        	System.out.println(java.util.Arrays.toString(array));
+	        }
+	     }
+	 }
 	
 	public static void selectionSort(int[] arrayToSort){
 		int minIndex=0;
@@ -57,5 +75,17 @@ public class SortingAlgorithms {
 	        }
 	System.out.println("Ends as "+Arrays.toString(array));
 	    }
+	}
+	public static void sort1(int [ ] num){            
+	     int i=0;
+
+	     for (int j = 1; j < num.length; j++){
+	           int key = num[j];
+	           for(i = j - 1; (i >= 0 && num[i] > key); i--){
+	                 num[i+1] = num[i];
+	          }
+	          num[i+1] = key;
+	           System.out.println(java.util.Arrays.toString(num));
+	     }
 	}
 }
