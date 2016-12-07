bc6c139ce46499c0b24e9657b91c03df5c4782e6
Student 8 <Student 8@10.8.33.147>
2016-12-07 14:01:08 -0500

    gggg

--- Period 8 Java/src/guiPratice/GUIApplication.java
+++ Period 8 Java/src/guiPratice/GUIApplication.java
@@ -3,7 +3,9 @@ package guiPratice;
 import javax.swing.JFrame;
 
 public class GUIApplication extends JFrame {
-
+	
+	private Screen currentScreen;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new GUIApplication();
@@ -16,6 +18,13 @@ public class GUIApplication extends JFrame {
 		int width = 600;
 		int height = 400;
 		setBounds(x, y, width, height);
+		initScreen();
 		setVisible(true);
+	}
+
+	protected void initScreen() {
+		// TODO Auto-generated method stub
+		Screen startScreen = new Screen(getWidth(), getHeight());
+		currentScreen = startScreen;
 	}
 }
